9c1e6087025a236312584db7b7d5a1b752b6a339
Kevin Andres Ortiz Merchan <kevinandresortizmerchan@Kevins-MacBook-Pro.local>
2016-01-07 02:35:42 -0500

    Add Fragment , Activities and packages

--- app/src/main/java/github/com/kevinortiz/attendanceapp/Activities/MainActivity.java
+++ app/src/main/java/github/com/kevinortiz/attendanceapp/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package github.com.kevinortiz.attendanceapp;
+package github.com.kevinortiz.attendanceapp.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import github.com.kevinortiz.attendanceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
